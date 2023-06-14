9bb122ad096cc7713c4533985be26a9b1e2f34c8
Hubinga <andischuster@gmx.at>
2023-06-14 23:33:53 +0200

    matrix multiplication

--- Calculator/Calculator/Classes/Matrix.cs
+++ Calculator/Calculator/Classes/Matrix.cs
@@ -8,14 +8,14 @@ namespace Calculator.Classes
 		public int RowSize { get; private set; }
 		public Tuple<int, int> Dimension => new Tuple<int, int>(RowSize, ColumnSize);
 
-		private int[,] matrix;
+		public int[,] MatrixBoard { get; private set; }
 
 		//filled matrix
 		public Matrix(int[,] matrixNumbers)
 		{
-			ColumnSize = matrixNumbers.GetLength(0);
-			RowSize = matrixNumbers.GetLength(1);
-			matrix = matrixNumbers;
+			RowSize = matrixNumbers.GetLength(0);
+			ColumnSize = matrixNumbers.GetLength(1);
+			MatrixBoard = matrixNumbers;
 		}
 
 		//empty matrix
@@ -23,27 +23,70 @@ namespace Calculator.Classes
 		{
 			RowSize = rowSize;
 			ColumnSize = columnSize;
-			matrix = new int[RowSize, ColumnSize];
+			MatrixBoard = new int[RowSize, ColumnSize];
 		}
 
 		public static Matrix operator+(Matrix a, Matrix b)
 		{
 			if(a.Dimension != b.Dimension)
 			{
-				throw new WrongDimesnionException("Beide MAtrizen müssen die selbe Dimension haben");
+				throw new WrongDimesnionException("Both matrices must have the same dimension!");
 			}
 
-			int[,] matrixNumbers = new int[a.RowSize, a.ColumnSize];
+			int[,] result = new int[a.RowSize, a.ColumnSize];
 
 			for (int i = 0; i < a.RowSize; i++)
 			{
 				for (int j = 0; j < a.ColumnSize; j++)
 				{
-					matrixNumbers[i, j] = a.matrix[i, j] + b.matrix[i, j];
+					result[i, j] = a.MatrixBoard[i, j] + b.MatrixBoard[i, j];
 				}
 			}
 			
-			return new Matrix(matrixNumbers);
+			return new Matrix(result);
+		}
+
+		public static Matrix operator *(Matrix a, Matrix b)
+		{
+			//e.g. (2,3) (3, 2)
+			//only defined, if column amount of A is equal row amount of B
+			if (a.ColumnSize != b.RowSize)
+			{
+				throw new WrongDimesnionException("Only defined, if column amount of A is equal row amount of B!");
+			}
+
+			//(m, n) * (n, r) => (m, r)
+			int[,] result = new int[a.RowSize, b.ColumnSize];
+
+			for (int i = 0; i < result.GetLength(0); i++)
+			{
+				for (int j = 0; j < result.GetLength(1); j++)
+				{
+					int currentElement = 0;
+
+					//k: from 0 to n - 1 (n = columnsize)
+					for (int k = 0; k < a.ColumnSize; k++)
+					{
+						currentElement += a.MatrixBoard[i, k] * b.MatrixBoard[k, j];
+					}
+
+					result[i, j] = currentElement;
+				}
+			}
+
+			return new Matrix(result);
+		}
+
+		public void Print()
+		{
+			for (int i = 0; i < RowSize; i++)
+			{
+				for (int j = 0; j < ColumnSize; j++)
+				{
+					Console.Write(MatrixBoard[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
 		}
 	}
 }
