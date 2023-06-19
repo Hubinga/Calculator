5175661799ad73d0a69a18e09af91f436138fb9d
Hubinga <andischuster@gmx.at>
2023-06-19 19:01:00 +0200

    calculate determinante

--- Calculator/Calculator/Classes/MatrixHelper.cs
+++ Calculator/Calculator/Classes/MatrixHelper.cs
@@ -28,9 +28,6 @@
 
 					matrix.ChangeTwoRows(rowIdx, rowIdxToChangeWith);
 					vector.ChangeTwoRows(rowIdx, rowIdxToChangeWith);
-                    Console.WriteLine("Change Row at Gaus-Jordan:");
-                    matrix.Print();
-					vector.Print();
 					continue;
 				}
 
@@ -69,11 +66,6 @@
 						}			
 					}
 				}
-
-                Console.WriteLine("current matrix A:");
-                matrix.Print();
-				Console.WriteLine("current vector b:");
-				vector.Print();
 				rowIdx++;
 			}
 
@@ -93,12 +85,6 @@
 			//round board values (avoid too many digits and very small values e.g. 5,1E-17)
 			vector.RoundBoardValues();
 			matrix.RoundBoardValues();
-
-			Console.WriteLine("current matrix A:");
-			matrix.Print();
-			Console.WriteLine("current vector b:");
-			vector.Print();
-			Console.WriteLine("end");
         }
 
 		private int GetRowIdx(Matrix m, int currentRowIdx, int columnIdx)
@@ -116,6 +102,93 @@
 			}
 
 			return -1;
+		}
+
+		public double CalculateDeterminante(Matrix matrix)
+		{
+			//start from first row (eliminate x1)
+			int rowIdx = 0;
+			double determinante = 1;
+
+			while (rowIdx < matrix.RowSize)
+			{
+				//determinante can be read from matrix if its is a triangular matrix
+				if (IsTriangularMatrix(matrix))
+				{
+					break;
+				}
+
+				//get x1, x2, ..., x(rowIdx) from current row
+				double currentX = matrix.MatrixBoard[rowIdx, rowIdx];
+
+				if (currentX == 0)
+				{
+					int rowIdxToChangeWith = GetRowIdx(matrix, rowIdx, rowIdx);
+
+					if (rowIdxToChangeWith == -1)
+					{
+						throw new GausJordanException("No row found to change with!");
+					}
+
+					matrix.ChangeTwoRows(rowIdx, rowIdxToChangeWith);
+					//determinante *= -1;
+					continue;
+				}
+
+				for (int i = 0; i < matrix.RowSize; i++)
+				{
+					if (i == rowIdx)
+					{
+						continue;
+					}
+
+					//get x1, x2, ... , x(rowIdx) from other rows
+					double xToEliminate = matrix.MatrixBoard[i, rowIdx];
+
+					//eliminate only if not already 0
+					if (xToEliminate != 0)
+					{
+						//xToEliminate + (currentX * x) = 0;
+						//e.g. 4 + (1 * x) = 0 -> 4 + x = 0 -> -4/1 = x -> x = -4 -> -(xToEliminate/currentX)
+						double factor = -(xToEliminate / currentX);
+
+						for (int j = 0; j < matrix.ColumnSize; j++)
+						{
+							matrix.MatrixBoard[i, j] += matrix.MatrixBoard[rowIdx, j] * factor;
+						}
+
+						//determinante *= factor;
+					}
+				}
+				rowIdx++;
+			}
+
+			//make I (diag(1,1,1,...))
+			for (int i = 0; i < matrix.RowSize; i++)
+			{
+				determinante *= matrix.MatrixBoard[i, i];
+			}
+
+			matrix.RoundBoardValues();
+
+            Console.WriteLine(determinante);
+            return determinante;
+		}
+
+		private bool IsTriangularMatrix(Matrix matrix)
+		{
+			for (int i = 0; i < matrix.RowSize; i++)
+			{
+				for (int j = i; j > 0; j--)
+				{
+					if (matrix.MatrixBoard[i,j] != 0)
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
 		}
 	}
 }
